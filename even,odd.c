827e393a8e28ab04fe39d9d830ab0da6261f7834
renuka <46022868+renuka2000@users.noreply.github.com>
2018-12-20 17:51:48 +0530

    Add files via upload
    to check whether the given number is even or odd

---
+++ even,odd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("Enter a number n: ");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("Given number is EVEN");
+}
+else
+{
+printf("Given number is ODD");
+}
+
+
+getch();
+}
